4f9b37f1a5b6e0a9268f58f384fac53017e5e6d5
Tim <Tim.salokat@outlook.com>
2021-04-08 16:46:02 +0200

    Copied all the files over

---
+++ TheGame/Assets/Scripts/playerHit.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class playerHit : MonoBehaviour
+{
+
+    public GameObject hit;
+    public float waitTillDisappear; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(HitFunction());
+        }
+    }
+
+    IEnumerator HitFunction()
+    {
+        hit.SetActive(true);
+        yield return new WaitForSeconds(waitTillDisappear);
+        hit.SetActive(false);
+    }
+}
